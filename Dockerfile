f49dd948f181381c5df5f453a7df7713629c3add
Cameron <c.bryce996@gmail.com>
2023-01-12 14:15:48 +0000

    More docker init and npm added

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
 FROM php:8.0-apache
 
-# Add RUN commands for extensions
+WORKDIR /var/www/html
+
+COPY package.json .
+
+RUN apt-get update && apt-get upgrade -y && \
+    apt-get install -y nodejs \
+    npm
+
+RUN npm install
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "Hello World!"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
